e0ca5c1b84d74fedb01cf4e4c509317b03ae25f5
Gabriel Wilkins <60405810+GNWilkins@users.noreply.github.com>
2020-04-19 22:46:06 -0400

    Add files via upload
    added fixed Milwaukee webmap

---
+++ 486_project_2_Wisconsin/New_Milwaukee_webmap/qgis2web_2020_04_19-22_35_28_353110/data/MilwaukeeApril2020PollingPlaces_5.js
@@ -0,0 +1,12 @@
+var json_MilwaukeeApril2020PollingPlaces_5 = {
+"type": "FeatureCollection",
+"name": "MilwaukeeApril2020PollingPlaces_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "display_na": "Hamilton High School, 6215, West Warnimont Avenue, Root Creek, Milwaukee, Milwaukee County, Wisconsin, 53220, United States of America" }, "geometry": { "type": "Point", "coordinates": [ -87.991373, 42.977381 ] } },
+{ "type": "Feature", "properties": { "display_na": "John Marshall High School, 4141, North 64th Street, Capitol Heights, Milwaukee, Milwaukee County, Wisconsin, 53216, United States of America" }, "geometry": { "type": "Point", "coordinates": [ -87.993239, 43.094307 ] } },
+{ "type": "Feature", "properties": { "display_na": "Riverside University High School, 1615, East Locust Street, Riverside Park, Milwaukee, Milwaukee County, Wisconsin, 53211, United States of America" }, "geometry": { "type": "Point", "coordinates": [ -87.89036, 43.070315 ] } },
+{ "type": "Feature", "properties": { "display_na": "Washington High School of Information Technology, 2525, North Sherman Boulevard, Washington Park, Milwaukee, Milwaukee County, Wisconsin, 53210, United States of America" }, "geometry": { "type": "Point", "coordinates": [ -87.968225, 43.065336 ] } },
+{ "type": "Feature", "properties": { "display_na": "South Division High School" }, "geometry": { "type": "Point", "coordinates": [ -87.931243, 43.013685 ] } }
+]
+}
